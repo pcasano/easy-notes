7c04d5617c7728202f3a095ca2047e0edb197d9e
pcasano <pc.learnings@pm.me>
2025-07-12 18:12:18 +0200

    add form

--- src/app/main-page/note/note.component.ts
+++ src/app/main-page/note/note.component.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 export type Note = {
   id: number;
@@ -8,10 +9,27 @@ export type Note = {
 
 @Component({
   selector: 'app-note',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './note.component.html',
   styleUrl: './note.component.scss',
 })
 export class NoteComponent {
   readonly selectedNote = input<Note | undefined>();
+
+  noteForm = new FormGroup({
+    title: new FormControl(''),
+    content: new FormControl(''),
+  });
+
+  readonly selectedNoteEffect = effect(() => {
+    const note = this.selectedNote();
+    if (note) {
+      this.noteForm.patchValue({
+        title: note.title,
+        content: note.content,
+      });
+    } else {
+      this.noteForm.reset();
+    }
+  });
 }
