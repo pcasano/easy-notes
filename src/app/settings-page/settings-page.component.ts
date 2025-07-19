da4a011457831c0c08cb50b93ab6f9199f96177b
pcasano <pc.learnings@pm.me>
2025-07-19 23:50:04 +0200

    add settings page and routing

--- src/app/settings-page/settings-page.component.ts
+++ src/app/settings-page/settings-page.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from 'express';
+import { SettingsStore } from './services/settings.store';
 
 @Component({
   selector: 'app-settings-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './settings-page.component.html',
-  styleUrl: './settings-page.component.scss'
+  styleUrl: './settings-page.component.scss',
 })
 export class SettingsPageComponent {
+  private router = inject(Router);
 
+  private settingsStore = inject(SettingsStore);
+
+  sortAlphabetically = true;
+  detectLinks = true;
+  showCreationDate = true;
+  showEditDate = true;
+  showDeleteDate = true;
+  activeArchive = true;
+
+  onBack() {
+    const settings = {
+      sortAlphabetically: this.sortAlphabetically,
+      detectLinks: this.detectLinks,
+      showCreationDate: this.showCreationDate,
+      showEditDate: this.showEditDate,
+      showDeleteDate: this.showDeleteDate,
+      activeArchive: this.activeArchive,
+    };
+
+    this.settingsStore.saveSettings(settings);
+
+    this.router.navigate(['/']);
+  }
 }
