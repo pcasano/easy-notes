ff15a98a8ba36c315bd0434d27bad56f07b17e06
pcasano <pc.learnings@pm.me>
2025-07-19 17:05:12 +0200

    add signalStore to manage application state

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,11 +1,11 @@
-import { Component, computed, signal, WritableSignal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { NotesPanelComponent } from './notes-panel/notes-panel.component';
 import { Note, NoteComponent } from './note/note.component';
-import { v4 as uuid } from 'uuid';
 import {
   NavigationBarComponent,
   Tab,
 } from './navigation-bar/navigation-bar.component';
+import { NoteStore } from './services/note.store';
 
 @Component({
   selector: 'app-main-page',
@@ -14,86 +14,30 @@ import {
   styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent {
-  notes: WritableSignal<Note[]> = signal([
-    {
-      id: '1',
-      title: 'note 1',
-      content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
-      createdAt: new Date(2015, 0, 11, 19, 25),
-      isNew: false,
-      tab: Tab.Notes,
-    },
-    {
-      id: '2',
-      title: 'note 2',
-      content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
-      createdAt: new Date(2017, 8, 7, 9, 45),
-      isNew: false,
-      tab: Tab.Notes,
-    },
-    {
-      id: '3',
-      title: 'note 3',
-      content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
-      createdAt: new Date(2014, 2, 15, 21, 30),
-      isNew: false,
-      tab: Tab.Notes,
-    },
-    {
-      id: '4',
-      title: 'note 4',
-      content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
-      createdAt: new Date(2019, 10, 3, 17, 5),
-      isNew: false,
-      tab: Tab.Notes,
-    },
-    {
-      id: '5',
-      title: 'note 5',
-      content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
-      createdAt: new Date(2016, 0, 28, 11, 50),
-      isNew: false,
-      tab: Tab.Trash,
-    },
-  ]);
+  private noteStore = inject(NoteStore);
 
-  selectedNote = signal<Note | undefined>(this.notes().at(0));
+  notes = this.noteStore.notes;
+
+  selectedTab = this.noteStore.selectedTab;
+
+  selectedNote = this.noteStore.selectedNote;
 
   filterText = signal('');
 
-  selectedTab = signal<Tab>(Tab.Notes);
-
   onNoteChosen(note: Note) {
-    this.selectedNote.set(note);
+    this.noteStore.onNoteSelected(note);
   }
 
   onNoteUpdated(updatedNote: Note) {
-    this.notes.update((notes) =>
-      notes.map((note) => (note.id === updatedNote.id ? updatedNote : note))
-    );
-    this.selectedNote.set(updatedNote);
+    this.noteStore.onNoteUpdated(updatedNote);
   }
 
   onNewNote() {
-    const newNote: Note = {
-      id: uuid(),
-      title: 'Untitled note',
-      createdAt: new Date(),
-      tab: Tab.Notes,
-    };
-    this.selectedNote.set(newNote);
-    this.notes.update((notes) => [...notes, newNote]);
+    this.noteStore.onNewNote();
   }
 
   onNoteMovedToTrash(noteToMoveToTrash: Note) {
-    this.notes.update((notes) =>
-      notes.map((note) =>
-        note.id === noteToMoveToTrash.id
-          ? { ...note, tab: Tab.Trash, movedToTrashAt: new Date() }
-          : note
-      )
-    );
-    this.selectNoteGivenTab(Tab.Notes);
+    this.noteStore.onNoteMovedToTrash(noteToMoveToTrash);
   }
 
   filteredNotes = computed(() => {
@@ -112,38 +56,14 @@ export class MainPageComponent {
   }
 
   onTabSelected(tab: Tab) {
-    this.selectedTab.set(tab);
-    this.selectedNote.set(
-      this.notes()
-        .filter((n) => n.tab === tab)
-        .at(0)
-    );
+    this.noteStore.onTabSelected(tab);
   }
 
   onNoteRestored(restoredNote: Note) {
-    this.notes.update((notes) =>
-      notes.map((note) =>
-        note.id === restoredNote.id
-          ? { ...note, tab: Tab.Notes, movedToTrashAt: undefined }
-          : note
-      )
-    );
-    this.selectNoteGivenTab(Tab.Trash);
+    this.noteStore.onNoteRestored(restoredNote);
   }
 
   onNoteDeleted(noteToDelete: Note) {
-    this.notes.update((notes) =>
-      notes.filter((note) => note.id !== noteToDelete.id)
-    );
-    this.selectNoteGivenTab(Tab.Trash);
-  }
-
-  selectNoteGivenTab(tab: Tab) {
-    const filteredNotes = this.notes().filter((n) => n.tab === tab);
-    if (filteredNotes.length > 0) {
-      this.selectedNote.set(filteredNotes.at(0));
-    } else {
-      this.selectedNote.set(undefined);
-    }
+    this.noteStore.onNoteDeleted(noteToDelete);
   }
 }
---
+++ src/app/main-page/services/note.store.ts
@@ -0,0 +1,146 @@
+import { Note } from '../note/note.component';
+import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { Tab } from '../navigation-bar/navigation-bar.component';
+import { v4 as uuid } from 'uuid';
+
+type NoteState = {
+  notes: Note[];
+  isLoading: boolean;
+  selectedNote: Note | undefined;
+  selectedTab: Tab;
+};
+
+const initialNotes = [
+  {
+    id: '1',
+    title: 'note 1',
+    content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
+    createdAt: new Date(2015, 0, 11, 19, 25),
+    tab: Tab.Notes,
+  },
+  {
+    id: '2',
+    title: 'note 2',
+    content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
+    createdAt: new Date(2017, 8, 7, 9, 45),
+    tab: Tab.Notes,
+  },
+  {
+    id: '3',
+    title: 'note 3',
+    content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
+    createdAt: new Date(2014, 2, 15, 21, 30),
+    tab: Tab.Notes,
+  },
+  {
+    id: '4',
+    title: 'note 4',
+    content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
+    createdAt: new Date(2019, 10, 3, 17, 5),
+    tab: Tab.Notes,
+  },
+  {
+    id: '5',
+    title: 'note 5',
+    content: 'asdasdsadsandalsdnaslkdalsdnaslkdasldasdsadasd8',
+    createdAt: new Date(2016, 0, 28, 11, 50),
+    tab: Tab.Trash,
+  },
+];
+
+const initialState: NoteState = {
+  notes: initialNotes,
+  isLoading: false,
+  selectedNote: initialNotes[0],
+  selectedTab: Tab.Notes,
+};
+
+export const NoteStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+  withMethods((store) => ({
+    onNewNote() {
+      const newNote: Note = {
+        id: uuid(),
+        title: 'Untitled note',
+        content: '',
+        createdAt: new Date(),
+        tab: Tab.Notes,
+      };
+      patchState(store, (state) => ({
+        ...state,
+        notes: [...state.notes, newNote],
+        selectedNote: newNote,
+      }));
+    },
+
+    onNoteUpdated(updatedNote: Note) {
+      patchState(store, (state) => ({
+        notes: state.notes.map((note) =>
+          note.id === updatedNote.id ? updatedNote : note
+        ),
+        selectedNote: updatedNote,
+      }));
+    },
+
+    onNoteSelected(note: Note) {
+      patchState(store, () => ({
+        selectedNote: note,
+      }));
+    },
+
+    onNoteMovedToTrash(noteToMoveToTrash: Note) {
+      patchState(store, (state) => {
+        const updatedNotes = state.notes.map((note) =>
+          note.id === noteToMoveToTrash.id
+            ? { ...note, tab: Tab.Trash, movedToTrashAt: new Date() }
+            : note
+        );
+
+        const filteredNotes = updatedNotes.filter((n) => n.tab === Tab.Notes);
+
+        return {
+          notes: updatedNotes,
+          selectedNote: filteredNotes.length > 0 ? filteredNotes[0] : undefined,
+        };
+      });
+    },
+
+    onNoteDeleted(noteToDelete: Note) {
+      patchState(store, (state) => {
+        const updatedNotes = state.notes.filter(
+          (note) => note.id !== noteToDelete.id
+        );
+        const filteredNotes = updatedNotes.filter((n) => n.tab === Tab.Trash);
+        return {
+          notes: updatedNotes,
+          selectedNote: filteredNotes.length > 0 ? filteredNotes[0] : undefined,
+        };
+      });
+    },
+
+    onNoteRestored(restoredNote: Note) {
+      patchState(store, (state) => {
+        const updatedNotes = state.notes.map((note) =>
+          note.id === restoredNote.id
+            ? { ...note, tab: Tab.Notes, movedToTrashAt: undefined }
+            : note
+        );
+
+        const filteredNotes = updatedNotes.filter((n) => n.tab === Tab.Trash);
+
+        return {
+          notes: updatedNotes,
+          selectedNote: filteredNotes[0],
+        };
+      });
+    },
+
+    onTabSelected(tab: Tab) {
+      patchState(store, (state) => ({
+        selectedTab: tab,
+        selectedNote: state.notes.find((note) => note.tab === tab),
+      }));
+    },
+  }))
+);
